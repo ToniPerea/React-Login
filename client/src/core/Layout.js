dee663d9c6917d5674d416be7ff4db9dcefc802d
ToniPerea <antonioperea31@gmail.com>
2021-06-29 12:43:55 +0200

    ActiveNav link(navbar interactivo)

--- client/src/core/Layout.js
+++ client/src/core/Layout.js
@@ -1,22 +1,32 @@
 import React, { Fragment } from "react";
-import {Link} from 'react-router-dom';
+import {Link, withRouter} from 'react-router-dom';
 
 
-const Layout = ({ children }) => {
+
+const Layout = ({ children, match, history }) => {
+  const isActive = path => {
+    if(match.path === path){
+      return {color: '#000'}
+    } else {
+      return {color: '#fff'}
+    }
+  }
+
+
   const nav = () => (
     <ul className="nav nav-tabs bg-primary">
       <li className="nav-item">
-        <Link to="/" className="text-light nav-link">
+        <Link to="/" className="nav-link" style={isActive('/')} >
           Home
         </Link>
       </li>
       <li className="nav-item">
-        <Link to="/signin" className="text-light nav-link">
+        <Link to="/signin" className="nav-link" style={isActive('/signin')} >
           Iniciar Sesión
         </Link>
       </li>
       <li className="nav-item">
-        <Link to="/signup" className="text-light nav-link">
+        <Link to="/signup" className="nav-link" style={isActive('/signup')} >
           Registrarse
         </Link>
       </li>
@@ -32,4 +42,4 @@ const Layout = ({ children }) => {
   );
 };
 
-export default Layout;
+export default withRouter(Layout);
